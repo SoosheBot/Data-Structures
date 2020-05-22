349fffb25081ce7aba68152ed53d99328e0ee75d
sooshe <sooshe@sooshebot.com>
2020-05-21 21:22:02 -0500

    debug doubly linked list

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,13 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = []
 
     def enqueue(self, value):
-        pass
+        self.storage.append(value)
 
     def dequeue(self):
-        pass
+        # if len()
 
     def len(self):
         pass
